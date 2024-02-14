8898d7556a31e3b2218494ae5ca2720f4ce5a221
atikin3008 <atikinvokmok@list.ru>
2024-02-14 23:56:04 +0300

    sockets

--- main.cpp
+++ main.cpp
@@ -11,10 +11,13 @@
 #include<sstream>
 #include <chrono>
 
+
 #define NOGUI false
 #define screenScale 300
 
-std::chrono::milliseconds pms = duration_cast<std::chrono::milliseconds>(
+std::string path = "/Users/nikitakomkov/CLionProjects/robofootball99/";
+#define FPS 60
+std::chrono::microseconds pms = duration_cast<std::chrono::microseconds>(
         std::chrono::system_clock::now().time_since_epoch()
 );
 
@@ -26,9 +29,41 @@ int get_sign(double x) {
     return 1;
 }
 
+struct Dot {
+    double x, y;
+
+    Dot(double _x = 0, double _y = 0) : x(_x), y(_y) {};
+
+    Dot getCoord(double mx, double my, double a) {
+        return Dot(mx + x * cos(-a) - y * sin(-a),
+                   my + x * sin(-a) + y * cos(-a));
+    }
+};
+
+struct Line {
+    double x1, y1, x2, y2;
+
+    Line(double _x1, double _y1, double _x2, double _y2) : x1(_x1), y1(_y1), x2(_x2), y2(_y2) {};
+
+    double operator!() const {
+        double res = sqrt((double) x1 * x1 + y1 * y1);
+        double res2 = sqrt((double) x2 * x2 + y2 * y2);
+        double res3 = (x1 * x2 + y1 * y2);
+        return acos((double) res3 / (res2 * res));
+    }
+
+};
+
 struct Robot {
-    double x = 0.1, y = 0.1, px = 0, py = 0, angle = 0;
+    double x = 0.1, y = 0.1, px = 0, py = 0, angle = 0, headSize = 0.07, bx, by;
+    std::vector<Dot> dots = {{0.095,  0.055},
+                             {0.095,  -0.055},
+                             {-0.095, -0.055},
+                             {-0.095, 0.055}};
+    std::vector<std::pair<std::string, Dot>> showDots = {};
+    Dot centralDot{0, 0.055};
     sf::Color color = sf::Color::White;
+    bool is_bounce = false;
 
     Robot() {};
 
@@ -57,85 +92,51 @@ struct Robot {
             y = maxY + 1.2;
     }
 
-    void socket(int z) {
-
+    void socket(int z, int maxX, int maxY) {
         TcpServerSocket server("localhost", z);
         server.acceptConnection();
         std::cout << z << "\n";
         std::cout << "OK\n";
         char o[100];
         while (true) {
+            std::string k;
+            Line line(x, y, bx, by);
+            if (fabsl(!line - angle) < M_PI / 4) {
+                k += "Ball: " + std::to_string(!line - angle + M_PI / 8) + "\n";
+            }
+            for(auto const &i : showDots){
+                Line line(x, y, i.second.x, i.second.y);
+                if (fabsl(!line - angle) < M_PI / 4) {
+                    k += i.first + ": " + std::to_string(!line - angle + M_PI / 8) + "\n";
+                }
+            }
+            char *kk;
+            strcpy(kk, k.c_str());
+            server.sendData(kk, 100);
             server.receiveData(o, 100);
             if (o[0] == 'q')
                 break;
             std::cout << o << "\n";
             std::stringstream s(o);
             double _x, _y, _a;
-            s >> _x >> _y >> _a;
-            x += _x;
-            y += _y;
-            angle += _a;
+            int _bounce;
+            s >> _x >> _y >> _a >> _bounce;
+
+            if (_bounce) {
+                is_bounce = true;
+            } else {
+                this->move(_x, _y, maxX, maxY);
+                this->rotate(_a);
+            }
         }
-
     }
 
 
 };
 
-struct Line {
-    double x1, y1, x2, y2;
-
-    Line(double X1, double Y1, double X2, double Y2) {
-        x1 = std::min(X1, X2);
-        x2 = std::max(X1, X2);
-        y1 = std::min(Y1, Y2);
-        y2 = std::max(Y1, Y2);
-    }
-
-    bool operator<(const Line l) const {
-        int sx1 = (x1 + x2) / 2;
-        int sx2 = (l.x1 + l.x2) / 2;
-        int sy1 = (y1 + y2) / 2;
-        int sy2 = (l.y1 + l.y2) / 2;
-        if (sx1 == sx2)
-            return sy1 < sy2;
-        return sx1 < sx2;
-    }
-
-    bool operator>(const Line l) const {
-        int sx1 = (x1 + x2) / 2;
-        int sx2 = (l.x1 + l.x2) / 2;
-        int sy1 = (y1 + y2) / 2;
-        int sy2 = (l.y1 + l.y2) / 2;
-        if (sx1 == sx2)
-            return sy1 > sy2;
-        return sx1 > sx2;
-    }
-
-    bool operator==(const Line l) const {
-        int sx1 = (x1 + x2) / 2;
-        int sx2 = (l.x1 + l.x2) / 2;
-        int sy1 = (y1 + y2) / 2;
-        int sy2 = (l.y1 + l.y2) / 2;
-        return sx1 == sx2 && sy1 == sy2;
-    }
-
-    bool operator!=(const Line l) const {
-        int sx1 = (x1 + x2) / 2;
-        int sx2 = (l.x1 + l.x2) / 2;
-        int sy1 = (y1 + y2) / 2;
-        int sy2 = (l.y1 + l.y2) / 2;
-        return !(sx1 == sx2 && sy1 == sy2);
-    }
-
-    friend std::ostream &operator<<(std::ostream &o, Line &l) {
-        o << l.x1 << " " << l.y1 << "|" << l.x2 << " " << l.y2 << std::endl;
-        return o;
-    }
-};
 
 struct Ball {
-    double x, y, ax, ay, sx, sy;
+    double x, y, ax, ay, sx, sy, s = 0.08;
     sf::Color color;
 
     Ball(double _x, double _y) : x(_x), y(_y) {}
@@ -144,8 +145,14 @@ struct Ball {
         bool check = false;
         x += ax;
         y += ay;
-        ax /= 1.1;
-        ay /= 1.1;
+        if (ax < 0)
+            ax += std::min(abs(ax), 0.0001);
+        else
+            ax -= std::min(abs(ax), 0.0001);
+        if (ay < 0)
+            ay += std::min(abs(ay), 0.0001);
+        else
+            ay -= std::min(abs(ay), 0.0001);
         /*
         if (check)
             std::cout << "\n";
@@ -153,34 +160,58 @@ struct Ball {
     }
 
     void check_robot(Robot &robot, int size) {
-        if (sqrt(pow(robot.x - x, 2) + pow(robot.y - y, 2)) <= (double) 15 / size) {
-            ax += (robot.x - robot.px) * 10;
-            ay += (robot.y - robot.py) * 10;
+        if (sqrt(pow(robot.x - x, 2) + pow(robot.y - y, 2)) <= robot.headSize + s) {
+            ax += (robot.x - robot.px);
+            ay += (robot.y - robot.py);
             //std::cout<<"YES|"<<ax<<"|"<<ay<<"\n";
         }
     }
 
-    void check_borders(int xSize, int ySize, int scale) {
-        if (x >= (double)xSize / scale + 0.1) {
-            ax *= -1;
-            ay *= 1;
-            std::cout<<"border\n";
+    void checkRobotPush(Robot &robot, int size) {
+        Dot c = robot.centralDot.getCoord(robot.x, robot.y, robot.angle);
+        if (sqrt(pow(c.x - x, 2) + pow(c.y - y, 2)) <= robot.headSize + s + 0.01) {
+            ax += 0.01 * cos(robot.angle - M_PI / 4) + 0.01 * sin(robot.angle - M_PI / 4);
+            ay += -0.01 * sin(robot.angle - M_PI / 4) + 0.01 * cos(robot.angle - M_PI / 4);
         }
-        if (y >= (double)ySize / scale + 0.1) {
-            ax *= 1;
-            ay *= -1;
-            std::cout<<"border\n";
+    }
+
+    std::pair<int, int> check_borders(int xSize, int ySize, int scale) {
+        std::pair l = {0, 0};
+        if (x >= (double) xSize / scale + 0.1) {
+            if (abs(y - (double) ySize / 2 / scale - (double) 60 / scale) <= 0.75) {
+                l.first = 1;
+            }
+            x = (double) xSize / scale / 2 + (double) 60 / scale;
+            ax = 0;
+            y = (double) ySize / scale / 2 + (double) 60 / scale;
+            ay = 0;
+            std::cout << "border 1\n";
+        }
+        if (y >= (double) ySize / scale + 0.1) {
+            x = (double) xSize / scale / 2 + (double) 60 / scale;
+            ax = 0;
+            y = (double) ySize / scale / 2 + (double) 60 / scale;
+            ay = 0;
+            std::cout << "border 2\n";
         }
         if (x <= 0.1) {
-            ax *= -1;
-            ay *= 1;
-            std::cout<<"border\n";
+            if (abs(y - (double) ySize / 2 / scale - (double) 60 / scale) <= 0.75) {
+                l.second = 1;
+            }
+            x = (double) xSize / scale / 2 + (double) 60 / scale;
+            ax = 0;
+            y = (double) ySize / scale / 2 + (double) 60 / scale;
+            ay = 0;
+            std::cout << "border 3\n";
         }
         if (y <= 0.1) {
-            ax *= 1;
-            ay *= -1;
-            std::cout<<"border\n";
+            x = (double) xSize / scale / 2 + (double) 60 / scale;
+            ax = 0;
+            y = (double) ySize / scale / 2 + (double) 60 / scale;
+            ay = 0;
+            std::cout << "border 4 \n";
         }
+        return l;
     }
 
 };
@@ -191,6 +222,8 @@ struct Field {
     int ySize;
     int scale;
     int indent = 60;
+    int c1 = 0;
+    int c2 = 0;
 
     Field(std::string fileName, int s = 500) {
         std::ifstream fin(fileName);
@@ -208,8 +241,8 @@ struct Field {
         json = nlohmann::json::parse(text);
         fin.close();
         std::cout << json << std::endl;
-        xSize = (int) (((float) json["FIELD_LENGTH"] - 0.6) * (float) scale);
-        ySize = (int) (((float) json["FIELD_WIDTH"] - 0.6) * (float) scale);
+        xSize = (int) (((float) json["FIELD_LENGTH"]) * (float) scale);
+        ySize = (int) (((float) json["FIELD_WIDTH"]) * (float) scale);
     }
 
     [[nodiscard]] sf::VideoMode getWindowSize() const {
@@ -229,20 +262,20 @@ struct Field {
         baseRect.setPosition(indent, indent);
         baseRect.setFillColor(sf::Color::Green);
         baseRect.setOutlineColor(sf::Color::White);
-        baseRect.setOutlineThickness(10);
+        baseRect.setOutlineThickness(0.05 * scale);
         window.draw(baseRect);
 
         sf::CircleShape middleCircle;
-        middleCircle.setRadius(50);
-        middleCircle.setPosition(xSize / 2 + indent - 50, ySize / 2 + indent - 50);
+        middleCircle.setRadius(0.5 * scale);
+        middleCircle.setPosition(xSize / 2 + indent - 0.5 * scale, ySize / 2 + indent - 0.5 * scale);
         middleCircle.setFillColor(sf::Color::Green);
         middleCircle.setOutlineColor(sf::Color::White);
-        middleCircle.setOutlineThickness(10);
+        middleCircle.setOutlineThickness(0.05 * scale);
         window.draw(middleCircle);
 
         sf::RectangleShape middleLine;
-        middleLine.setSize(sf::Vector2f(10, ySize));
-        middleLine.setPosition(xSize / 2 + indent - 5, indent);
+        middleLine.setSize(sf::Vector2f(0.05 * scale, ySize));
+        middleLine.setPosition(xSize / 2 + indent - 0.025 * scale, indent);
         middleLine.setFillColor(sf::Color::White);
         middleLine.setOutlineColor(sf::Color::White);
         middleLine.setOutlineThickness(0);
@@ -284,8 +317,14 @@ struct Field {
 
     void drawRobot(Robot &robot, sf::RenderWindow &window) {
         sf::CircleShape R;
-        R.setRadius(10);
-        R.setPosition(robot.x * scale - 10, robot.y * scale - 10);
+        R.setRadius(12);
+        Dot c = robot.centralDot.getCoord(robot.x, robot.y, robot.angle);
+        R.setPosition(c.x * scale - 12, c.y * scale - 12);
+        R.setFillColor(sf::Color::Magenta);
+        R.setOutlineColor(robot.color);
+        window.draw(R);
+        R.setRadius(robot.headSize * scale);
+        R.setPosition(robot.x * scale - robot.headSize * scale, robot.y * scale - robot.headSize * scale);
         R.setFillColor(robot.color);
         window.draw(R);
         sf::VertexArray line(sf::Lines, 2);
@@ -295,21 +334,58 @@ struct Field {
         line[0].color = robot.color;
         line[1].color = sf::Color::Red;
         window.draw(line);
+        for (int i = 0; i < 3; ++i) {
+            Dot x = robot.dots[i].getCoord(robot.x, robot.y, robot.angle);
+            Dot x1 = robot.dots[i + 1].getCoord(robot.x, robot.y, robot.angle);
+            //std::cout << i << "|" << i + 1 << "\n";
+
+            sf::CircleShape R;
+            R.setRadius(6);
+            R.setPosition(x.x * scale - 6, x.y * scale - 6);
+            R.setFillColor(sf::Color::Magenta);
+            //window.draw(R);
+            sf::VertexArray line(sf::Lines, 2);
+            line[0] = sf::Vector2f(x.x * scale, x.y * scale);
+            line[1] = sf::Vector2f(x1.x * scale, x1.y * scale);
+            line[0].color = robot.color;
+            line[1].color = sf::Color::Red;
+            window.draw(line);
+        }
+        Dot x = robot.dots[3].getCoord(robot.x, robot.y, robot.angle);
+        Dot x1 = robot.dots[0].getCoord(robot.x, robot.y, robot.angle);
+        line[0] = sf::Vector2f(x.x * scale, x.y * scale);
+        line[1] = sf::Vector2f(x1.x * scale, x1.y * scale);
+        line[0].color = robot.color;
+        line[1].color = sf::Color::Red;
+        window.draw(line);
+
 
     }
 
     void drawBall(Ball &ball, sf::RenderWindow &window) {
         sf::CircleShape R;
-        R.setRadius(5);
-        R.setPosition(ball.x * scale - 10, ball.y * scale - 10);
+        R.setRadius(ball.s * scale);
+        R.setPosition(ball.x * scale - ball.s * scale, ball.y * scale - ball.s * scale);
         R.setFillColor(ball.color);
         window.draw(R);
     }
 
+    void drawText(sf::RenderWindow &window) {
+        sf::Font font;
+        font.loadFromFile(path + "arialmt.ttf");
+        sf::Text text;
+        text.setFont(font);
+        text.setString(std::to_string(c1) + ":" + std::to_string(c2));
+        text.setCharacterSize(72);
+        text.setFillColor(sf::Color::Red);
+        text.setPosition(xSize / 2 + indent - 54, 50);
+        window.draw(text);
+    }
+
 };
 
-void Socket(Robot &robot, int z) {
-    robot.socket(z);
+void Socket(Robot &robot, int z, int xSize, int Ysize) {
+    robot.socket(z, xSize, Ysize);
 }
 
 int main() {
@@ -322,12 +398,12 @@ int main() {
     robot1.color = sf::Color::Blue;
     //std::thread t(Socket, std::ref(robot), 5040);
 
-    Ball ball(0.2, 0.2);
+    Ball ball(0.5, 0.5);
     //ball.ax = 0.1;
     //sball.ay = 0.1;
-    ball.color = sf::Color::Yellow;
-    while (window.isOpen()) {
+    ball.color = sf::Color::Magenta;
 
+    while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
@@ -352,30 +428,41 @@ int main() {
                 if (event.key.code == sf::Keyboard::Left) {
                     robot.rotate(0.05);
                 }
+                if (event.key.code == sf::Keyboard::Space) {
+                    robot.is_bounce = true;
+                }
 
             }
 
         }
-
+        if (robot.is_bounce) {
+            robot.is_bounce = false;
+            ball.checkRobotPush(robot, field.scale);
+        }
         field.drawField(window);
         field.drawRobot(robot, window);
         field.drawRobot(robot1, window);
         ball.tick();
         ball.check_robot(robot, field.scale);
+        robot.x = ball.x;
+        robot.y = ball.y;
         field.drawBall(ball, window);
-        ball.check_borders(field.xSize, field.ySize, field.scale);
+        auto m = ball.check_borders(field.xSize, field.ySize, field.scale);
+        field.c1 += m.first;
+        field.c2 += m.second;
+        field.drawText(window);
         window.display();
 
-        std::chrono::milliseconds ms = duration_cast<std::chrono::milliseconds>(
+        std::chrono::microseconds ms = duration_cast<std::chrono::microseconds>(
                 std::chrono::system_clock::now().time_since_epoch()
         );
-        while ((ms - pms).count() <= 1000 / 60) {
-            ms = duration_cast<std::chrono::milliseconds>(
-                    std::chrono::system_clock::now().time_since_epoch()
-            );
 
+        while ((ms - pms).count() < 1000000 / FPS) {
+            ms = duration_cast<std::chrono::microseconds>(
+                    std::chrono::system_clock::now().time_since_epoch());
         }
-        pms = duration_cast<std::chrono::milliseconds>(
+        //std::cout << 1000000 / (ms - pms).count()<<"\n";
+        pms = duration_cast<std::chrono::microseconds>(
                 std::chrono::system_clock::now().time_since_epoch()
         );
 
